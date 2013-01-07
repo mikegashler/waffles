b74c3e5a5cc5851ba4691139a2b9cc1ed1788a53
Mike Gashler <mikegashler@gmail.com>
2013-01-07 12:05:19 -0600

    add a file that was previously neglected

--- src/depends/security/GKeyboard.cpp
+++ src/depends/security/GKeyboard.cpp
@@ -159,21 +159,6 @@ void GKeyboard::Watch()
 #include "GKeyboard.h"
 #include <GClasses/GError.h>
 #include <stdio.h>
-#include <openssl/md4.h>
-//#include <X11/Intrinsic.h>
-
-extern "C" {
-int MD4_Init(MD4_CTX *c);
-int MD4_Update(MD4_CTX *c, const void *data, size_t len);
-int MD4_Final(unsigned char *md, MD4_CTX *c);
-unsigned char *MD4(const unsigned char *d, size_t n, unsigned char *md);
-void MD4_Transform(MD4_CTX *c, const unsigned char *b);
-}
-
-
-
-
-
 #include <X11/StringDefs.h>
 #include <X11/Xutil.h>
 #include <X11/Shell.h>
