75b642712d92ea25d65746b7f8c7708e80dbdff9
Mike Gashler <mikegashler@gmail.com>
2015-04-22 22:42:42 -0500

    Add the time-series demo. (It's a spin-off of the fourier-net demo, but simpler, easier to modify, faster, and gets better results.)

---
+++ demos/time_series/src/main.cpp
@@ -0,0 +1,182 @@
+/*
+  The contents of this file are dedicated by all of its authors, including
+
+    Michael S. Gashler,
+    anonymous contributors,
+
+  to the public domain (http://creativecommons.org/publicdomain/zero/1.0/).
+
+  Note that some moral obligations still exist in the absence of legal ones.
+  For example, it would still be dishonest to deliberately misrepresent the
+  origin of a work. Although we impose no legal requirements to obtain a
+  license, it is beseeming for those who build on the works of others to
+  give back useful improvements, or pay it forward in their own field. If
+  you would like to cite us, a published paper about Waffles can be found
+  at http://jmlr.org/papers/volume12/gashler11a/gashler11a.pdf. If you find
+  our code to be useful, the Waffles team would love to hear how you use it.
+*/
+
+#include <exception>
+#include <iostream>
+#include <GClasses/GApp.h>
+#include <GClasses/GError.h>
+#include <GClasses/GMatrix.h>
+#include <GClasses/GFourier.h>
+#include <GClasses/GNeuralNet.h>
+#include <GClasses/GActivation.h>
+#include <GClasses/GPlot.h>
+#include <GClasses/GVec.h>
+#include <GClasses/GHillClimber.h>
+#include <math.h>
+#include <fstream>
+
+using namespace GClasses;
+using std::cerr;
+using std::cout;
+using std::string;
+
+#define SOFTPLUS_NODES 12
+#define IDENTITY_NODES 12
+#define SOFTPLUS_SHIFT 10
+#define PERTURBATION 1e-4
+#define TRAINING_EPOCHS 100000
+#define REGULARIZATION_TERM 0.5
+#define LEARNING_RATE 1e-3
+
+void plot_it(const char* filename, GNeuralNet& nn, GMatrix& trainFeat, GMatrix& trainLab, GMatrix& testFeat, GMatrix& testLab)
+{
+	GSVG svg(1000, 500);
+	svg.newChart(0.0, std::min(trainLab.columnMin(0), testLab.columnMin(0)), 2.0, std::max(trainLab.columnMax(0), testLab.columnMax(0)));
+	svg.horizMarks(20);
+	svg.vertMarks(20);
+	double prevx = 0.0;
+	double prevy = 0.0;
+	for(double x = prevx; x < 2.0; x += 0.002)
+	{
+		double y;
+		nn.predict(&x, &y);
+		if(prevx != x)
+			svg.line(prevx, prevy, x, y, 0.3);
+		prevx = x;
+		prevy = y;
+	}
+	for(size_t i = 0; i < trainLab.rows(); i++)
+		svg.dot(trainFeat[i][0], trainLab[i][0], 0.4, 0xff000080);
+	for(size_t i = 0; i < testLab.rows(); i++)
+		svg.dot(testFeat[i][0], testLab[i][0], 0.4, 0xff800000);
+
+	std::ofstream ofs;
+	ofs.open(filename);
+	svg.print(ofs);
+}
+
+
+void doit()
+{
+	// Load the data
+	GMatrix trainLab;
+	GMatrix testLab;
+	trainLab.loadArff("train.arff");
+	testLab.loadArff("test.arff");
+	GMatrix trainFeat(trainLab.rows(), 1);
+	for(size_t i = 0; i < trainLab.rows(); i++)
+		trainFeat[i][0] = (double)i / trainLab.rows();
+	GMatrix testFeat(testLab.rows(), 1);
+	for(size_t i = 0; i < testLab.rows(); i++)
+		testFeat[i][0] = (double)(i + trainLab.rows()) / trainLab.rows();
+
+	// Make a neural network
+	GNeuralNet nn;
+	GUniformRelation relOne(1);
+	nn.beginIncrementalLearning(relOne, relOne);
+
+	// Initialize the weights of the sine units to match the frequencies used by the Fourier transform.
+	GLayerClassic* pSine2 = new GLayerClassic(1, trainLab.rows(), new GActivationSin());
+	GMatrix& wSin = pSine2->weights();
+	double* bSin = pSine2->bias();
+	double period = 1.0;
+	for(size_t i = 0; i < trainLab.rows() / 2; i++)
+	{
+		wSin[0][2 * i] = 2.0 * M_PI * (i + 1) / period;
+		bSin[2 * i] = 0.5 * M_PI;
+		wSin[0][2 * i + 1] = 2.0 * M_PI * (i + 1) / period;
+		bSin[2 * i + 1] = M_PI;
+	}
+
+	// Make the hidden layer
+	GLayerMixed* pMix2 = new GLayerMixed();
+	pSine2->resize(1, pSine2->outputs(), &nn.rand(), PERTURBATION);
+	pMix2->addComponent(pSine2);
+	GLayerClassic* pSoftPlus2 = new GLayerClassic(1, SOFTPLUS_NODES, new GActivationSoftPlus());
+	pMix2->addComponent(pSoftPlus2);
+	GLayerClassic* pIdentity2 = new GLayerClassic(1, IDENTITY_NODES, new GActivationIdentity());
+	pMix2->addComponent(pIdentity2);
+	nn.addLayer(pMix2);
+
+	// Make the output layer
+	GLayerClassic* pIdentity3 = new GLayerClassic(FLEXIBLE_SIZE, trainLab.cols(), new GActivationIdentity());
+	pIdentity3->resize(pMix2->outputs(), pIdentity3->outputs(), &nn.rand(), PERTURBATION);
+	nn.addLayer(pIdentity3);
+
+	// Initialize all the non-periodic nodes to approximate the identity function, then perturb a little bit
+	pSoftPlus2->setWeightsToIdentity();
+	for(size_t i = 0; i < SOFTPLUS_NODES; i++)
+	{
+		pSoftPlus2->bias()[i] += SOFTPLUS_SHIFT;
+		pIdentity3->renormalizeInput(pSine2->outputs() + i, 0.0, 1.0, SOFTPLUS_SHIFT, SOFTPLUS_SHIFT + 1.0);
+	}
+	pIdentity2->setWeightsToIdentity();
+	pSoftPlus2->perturbWeights(nn.rand(), PERTURBATION);
+	pIdentity2->perturbWeights(nn.rand(), PERTURBATION);
+
+	// Randomly initialize the weights on the output layer
+	pIdentity3->weights().setAll(0.0);
+	pIdentity3->perturbWeights(nn.rand(), PERTURBATION);
+
+	// Do some training
+	GRandomIndexIterator ii(trainLab.rows(), nn.rand());
+	nn.setLearningRate(LEARNING_RATE);
+	for(size_t epoch = 0; epoch < TRAINING_EPOCHS; epoch++)
+	{
+		// Visit each sample in random order
+		ii.reset();
+		size_t i;
+		while(ii.next(i))
+		{
+			// Regularize
+			pIdentity3->scaleWeights(1.0 - 0.01 * nn.learningRate() * REGULARIZATION_TERM, true);
+			pIdentity3->diminishWeights(0.01 * nn.learningRate() * REGULARIZATION_TERM, true);
+
+			// Train
+			nn.trainIncremental(trainFeat[i], trainLab[i]); // One iteration of stochastic gradient descent
+		}
+
+		// Report progress
+		double rmse = sqrt(nn.sumSquaredError(trainFeat, trainLab) / trainLab.rows());
+		if(epoch % (TRAINING_EPOCHS / 100) == 0)
+		{
+			double val = sqrt(nn.sumSquaredError(testFeat, testLab) / testLab.rows());
+			cout << "prog=" << to_str((double)epoch * 100.0 / TRAINING_EPOCHS) << "%	rmse=" << to_str(rmse) << "	val=" << to_str(val) << "\n";
+			plot_it("progress.svg", nn, trainFeat, trainLab, testFeat, testLab);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+#ifdef _DEBUG
+	GApp::enableFloatingPointExceptions();
+#endif
+	int nRet = 0;
+	try
+	{
+		doit();
+	}
+	catch(const std::exception& e)
+	{
+		cerr << e.what() << "\n";
+		nRet = 1;
+	}
+
+	return nRet;
+}
